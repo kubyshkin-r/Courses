457c688804a8e016b5ef77e10e39991ac34ff032
a.cheprasov <a.cheprasov@intermeda.ru>
2022-06-01 13:01:27 +0300

    Ответ на вопрос в пятом задании

--- Module-1-Basic-JS/5-String/index2.js
+++ Module-1-Basic-JS/5-String/index2.js
@@ -18,8 +18,13 @@ console.log(myInfoText[0], myInfoText[myInfoText.length - 1]);
 //3 и 4
 let userName = prompt('Как зовут?');
     userName = userName.toLowerCase().trim();
-let userAge = prompt('Сколько вам лет?');  /* можно ли объявить переменную и сразу привести её к Number в одной строке */
+let userAge = prompt('Сколько вам лет?');  /* можно ли объявить переменную и сразу привести её к Number в одной строке */  
     userAge = Number(userAge.replaceAll(' ', '')); /*сначала сделал .trim() но так проверка совершенней, тк удаляет проблы в середине строки*/
 alert(`Вас зовут ${userName} и вам ${userAge} лет`);
 
 
+// Ответы на вопросы:
+/* можно ли объявить переменную и сразу привести её к Number в одной строке 
+Ответ: Можно. let userName = Number('123') сразу станет переменной с числом 
+
+*/
